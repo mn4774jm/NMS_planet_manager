61804a5f01c391a2efe5bcbef8a47b8caa82d51c
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-05-05 16:31:05 -0500

    Having issues when creating the sqlite file. Still having issues with DB. Created links to other sections of the page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="footer">
-        <div class="m-2">
-            <span class="p-3">
-                <router-link to="/">Submit Planet</router-link>
-            </span>
+<!--        <div class="m-2">-->
+<!--            <span class="p-3">-->
+<!--                <router-link to="/">Submit Planet</router-link>-->
+<!--            </span>-->
 
-            <span class="p-3">
-                <router-link to="/browse">Browse Worlds</router-link>
-            </span>
+<!--            <span class="p-3">-->
+<!--                <router-link to="/browse">Browse Worlds</router-link>-->
+<!--            </span>-->
 
-            <span class="p-3">
-                <router-link to="/search">Search Worlds</router-link>
-            </span>
+<!--            <span class="p-3">-->
+<!--                <router-link to="/search">Search Worlds</router-link>-->
+<!--            </span>-->
 
-            <span class="p-3">
-                <router-link to="/about">About</router-link>
-                </span>
-        </div>
+<!--            <span class="p-3">-->
+<!--                <router-link to="/about">About</router-link>-->
+<!--                </span>-->
+<!--        </div>-->
 
         <p class="text-right p-2">Header image by
             <a href="https://pngtree.com/free-backgrounds">free background photos from pngtree.com</a>
