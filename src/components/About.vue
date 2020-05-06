8e0e0c240314aabcc5d10770193e659acc61fa7b
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-05-06 09:18:55 -0500

    Database not connected yet. TODO build search page and connect to api TODO validation for dropdown menus on NewPlanetForm TODO loop for dropdown options to clean up code TODO look into inline format for dropdown menus

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="about">
         <h2>No Man's Sky Planet Archive</h2>
-        <p>WIP program to store planetary details for players of the game No Man's Sky by HelloGames</p>
+        <p>WIP database site to store planetary details for players of the game No Man's Sky by HelloGames</p>
         <p>A Vue.js site by Thomas Mullins</p>
     </div>
 </template>
